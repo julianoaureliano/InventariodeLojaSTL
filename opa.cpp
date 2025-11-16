58dd9d008e928de30c8cdcf28896527a7ac43174
JulianoAureliano <juliano.aureliano2013@gmail.com>
2025-11-16 15:56:35 -0300

    Adicionando os map Strings

--- opa.cpp
+++ opa.cpp
@@ -27,6 +27,7 @@ public:
 int main()
 {
     vector<Produto> produtos;
+    set<string> categoriaUnica; // set categoria unica
 
     // adicionando produtos
     produtos.push_back(Produto("Camisa", "Vestuario", 79.90, 30));
@@ -38,6 +39,39 @@ int main()
     for (const auto &opa : produtos)
     {
         opa.exibir();
+    }
+    // map<string, int>: contagem de produtos por categoria
+    map<string, int> quantidadeEmEstoque;
+    for (auto &p : produtos)
+    {
+        quantidadeEmEstoque[p.categoria] += p.estoque;
+    }
+    cout << "\nQuantidade Total do estoque por Categoria:\n";
+    for (const auto &par : quantidadeEmEstoque)
+    {
+        cout << "Categoria: " << par.first << " | Quantidade Total:  " << par.second << endl;
+    }
+    // map<string, double>: valor total em estoque por categoria
+    map<string, double> valorTotalEmEstoque;
+    for (auto &p : produtos)
+    {
+        valorTotalEmEstoque[p.categoria] += p.preco * p.estoque;
+    }
+    cout << "\nValor Total por Categoria:\n";
+    for (const auto &par : valorTotalEmEstoque)
+    {
+        cout << "Categoria: " << par.first << " | Valor Total: R$ " << par.second << endl;
+    }
+
+    // set: categorias unicas de produtos
+    for (const Produto &produto : produtos)
+    {
+        categoriaUnica.insert(produto.categoria);
+    }
+    cout << "\n--- CATEGORIAS UNICAS (set) ---" << endl;
+    for (const string &unico : categoriaUnica)
+    {
+        cout << "- " << unico << endl;
     }
     return 0;
 }
